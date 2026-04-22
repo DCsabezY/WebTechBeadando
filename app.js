f28d8e9c009a0403994bb60eacc8815009048219
CsabezY <d.csaba2004@gmail.com>
2026-04-22 23:22:33 +0200

    v2
    3-as szint

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const API_BASE = 'https://iit-playground.arondev.hu/api';
 
+const CAR_BRANDS = [
+    'Toyota','Honda','Ford','Chevrolet','Nissan',
+    'BMW','Mercedes-Benz','Volkswagen','Audi','Hyundai',
+    'Kia','Subaru','Lexus','Mazda','Tesla',
+    'Jeep','Porsche','Volvo','Jaguar','Land Rover',
+    'Mitsubishi','Ferrari','Lamborghini'
+];
+
 function loadNavigation(){
     fetch('./navbar.html')
         .then(res => res.text())
@@ -8,6 +16,9 @@ function loadNavigation(){
         
             if (getNeptun()) {
                 document.getElementById('navbar-right').classList.remove('hidden');
+            }
+            if (window.location.pathname.endsWith('cars.html')) {
+                document.getElementById('new-car-btn').classList.remove('hidden');
             }
         })
         
@@ -42,6 +53,36 @@ function getCars(neptun) {
 function getCarDetails(neptun, id) {
     return fetch(`${API_BASE}/${neptun}/car/${id}`)
         .then(res => res.json());
+}
+
+function createCar(neptun, carData) {
+    return fetch(`${API_BASE}/${neptun}/car`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(carData)
+    }).then(res => res.json());
+}
+
+function fillBrandSelect(selectId) {
+    const select = document.getElementById(selectId);
+    CAR_BRANDS.forEach(brand => {
+        select.innerHTML += `<option value="${brand}">${brand}</option>`;
+    });
+}
+
+function deleteCar(neptun, id) {
+    return fetch(`${API_BASE}/${neptun}/car/${id}`, {
+        method: 'DELETE'
+    }).then(res => res.json());
+
+}
+
+function updateCar(neptun, id, carData) {
+    return fetch(`${API_BASE}/${neptun}/car`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(carData)
+    }).then(res => res.json());
 }
 
 loadNavigation();
